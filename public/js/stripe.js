443da84ce297ef5568ef95ad9f2d6af78cada724
Quang Tran <tranminhquang0907@gmail.com>
2019-09-22 15:19:00 +0700

    Prepared app for deployment

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -7,10 +7,8 @@ export const bookTour = async tourId => {
   //tourId comes from the (data-tour-id) on the pug template, and then is read in index.js whenever someone hits the booking button
   try {
     // 1) Get checkout session from API
-    const session = await axios(
-      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
-    );
-    console.log(session);
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
+    // console.log(session);
 
     // 2) Create checkout form + charge credit card
     await stripe.redirectToCheckout({
