82050facf1ffd5b51c67a8e3c08c0f40fde0568a
Quang Tran <tranminhquang0907@gmail.com>
2019-09-25 16:24:14 +0700

    Implemented CORS

--- server.js
+++ server.js
@@ -39,6 +39,6 @@ process.on("unhandledRejection", err => {
 process.on("SIGTERM", () => {
   console.log("👋 SIGTERM RECEIVED. Shutting down gracefully");
   server.close(() => {
-    console.log("💥 Process terminated!");
+    console.log("💥 Process terminated!"); // Don't close the process abruptly
   });
 });
